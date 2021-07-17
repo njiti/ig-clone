880bb278c13dbc5cfbaa0475f9f6b0571f795d7b
njiti <kamaukan77@gmail.com>
2021-07-17 12:45:20 -0930

    Wrote a list of what technologies are being used

---
+++ test.js
@@ -0,0 +1,10 @@
+//Tech used in building app
+
+//React + React Hooks
+//React context API --> REDUX pattern
+//Material UI
+//FlexBox
+//Firebase's FireStore realtime DB
+//Firebase Free Hosting
+//Firebase Google Authentication (FULL LOGIN!)
+//React flip move
